9339c7a7d4663ebff49513e09e79ad1ce9b519b0
SahilUsmani <u.sahil.co@gmail.com>
2022-12-07 11:27:38 +0530

    Added two different header file and library named arith.h libarith.a(which contains function of multiplication and division) and arithmat.h libarithmat.a(which contains function of modulus and square) from Tejaswani and Hiya respectively

---
+++ Day01/library/arithmat.h
@@ -0,0 +1,27 @@
+/****************************************************************************************
+
+Author Name - Hiya Pandey
+Department - PG-DESD
+
+*****************************************************************************************/
+
+/*****************************************************************************************
+
+Function Name - mod
+Parameter 01 - Integer type
+Parameter 02 - Integer type
+Return Type - Integer
+Desciption - This function takes two arguments and performs arithmetic modulus on them. 
+
+******************************************************************************************/
+int mod(int,int);
+
+/*****************************************************************************************
+
+Function Name - sqr
+Parameter 01 - Integer type
+Return Type - Integer
+Desciption - This function takes one argument and performs square operation on them. 
+
+******************************************************************************************/
+int sqr(int);
