c7f0a8b89744a237032742758708dfac3f8bf86e
SahilUsmani <u.sahil.co@gmail.com>
2022-12-08 18:13:03 +0530

    Created a Dynamic Library(Shared Object)

---
+++ Day03/library_so/arithmetic.h
@@ -0,0 +1,30 @@
+
+
+/****************************************************************************************
+
+Author Name - Sahil Usmani
+Department - PG-DESD Student
+
+*****************************************************************************************/
+
+/*****************************************************************************************
+
+Function Name - add
+Parameter 01 - Integer type
+Parameter 02 - Integer type
+Return Type - Integer
+Desciption - This function takes two arguments and performs arithmetic addition on them. 
+
+******************************************************************************************/
+int add(int, int);
+
+/*****************************************************************************************
+
+Function Name - sub
+Parameter 01 - Integer type
+Parameter 02 - Integer type
+Return Type - Integer
+Desciption - This function takes two arguments and performs arithmetic subtraction on them. 
+
+******************************************************************************************/
+int sub(int, int);
